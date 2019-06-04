063e8a03804070c9bbbfacba504b3c4efe117064
Nipun Jindal <t-nijind@microsoft.com>
2019-06-04 15:26:09 -0700

    angular boilerplate

---
+++ src/constants.ts
@@ -0,0 +1,18 @@
+﻿export const CONSTANTS = {
+  ERROR_MESSAGE: {
+    MASTERDETAIL_GET : 'Request to get master detail text failed:',
+    LIST_DELETE : 'Request to delete list item failed:',
+    LIST_ADD : 'Request to add list item failed:',
+    LIST_GET : 'Request to get list items failed:',
+    LIST_EMPTY_MESSAGE : 'Please enter a valid message',
+    GRID_GET : 'Request to get grid text failed:',
+
+  },
+
+  ENDPOINT: {
+    MASTERDETAIL : '/api/masterdetail',
+    LIST : '/api/list',
+    GRID : '/api/grid',
+    
+  }
+};
